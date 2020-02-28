bc314ab8a7cd99c54e03e15c361dcd7308ebc897
Amanieu d'Antras <amanieu@gmail.com>
2020-02-28 18:58:50 +0000

    Disable custom_unwind_resume on Windows

--- src/librustc_target/spec/windows_base.rs
+++ src/librustc_target/spec/windows_base.rs
@@ -64,7 +64,7 @@ pub fn opts() -> TargetOptions {
         ],
         late_link_args,
         post_link_objects: vec!["rsend.o".to_string()],
-        custom_unwind_resume: true,
+        custom_unwind_resume: false,
         abi_return_struct_as_int: true,
         emit_debug_gdb_scripts: false,
         requires_uwtable: true,
